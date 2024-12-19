9183eda4ee72be7d5a8fda2ff5429766d4053293
alli-Codes <allisonevans081@gmail.com>
2024-12-19 13:38:16 +0100

    feat: make nav links to be active based on the page viewed

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,16 @@
+<script setup>
+    const route = useRoute()
+    const currentRoute = computed(() => route.name)
+</script>
+
 <template>
     <header class="bg-white px-32 py-4 flex items-center  justify-center sticky top-0 z-20">
        <div class="w-full flex  justify-center items-center">
           <Logo />
           <nav class="flex justify-center gap-12 w-full">
-             <NuxtLink to="/" class="active">Home</NuxtLink>
-             <NuxtLink to="about">About</NuxtLink>
-             <NuxtLink to="contact">Contact</NuxtLink>
+             <NuxtLink to="/" :class="currentRoute === 'index' ? 'active' : undefined">Home</NuxtLink>
+             <NuxtLink to="about" :class="currentRoute === 'about' ? 'active' : undefined">About</NuxtLink>
+             <NuxtLink to="contact" :class="currentRoute === 'contact' ? 'active' : undefined">Contact</NuxtLink>
           </nav>
           <section class="flex items-center justify-end w-full gap-12">
              <NuxtLink href="#" class="text-[#D90000]">Get Tickets</NuxtLink>
