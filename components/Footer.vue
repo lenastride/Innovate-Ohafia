f02ac2279e4858ab8f0c6e455986831b415c374f
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2025-02-04 07:56:30 +0100

    worked on the Footer component

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,1 +1,51 @@
-<template></template>
+<template>
+    <footer class="bg-[#0072B5] w-full pt-5 items-center flex flex-col">
+        <div class="w-full flex justify-center">
+            <div class="p-5 pb-0 text-white w-full md:max-w-[100em] list-none flex flex-col md:flex-row md:justify-between">
+                <section class="w-full ">
+                    <span class="pb-8 block">
+                        <DesktopLogo/>
+                    </span>
+                    <p class="pb-10 max-w-[25em]">
+                        Igniting youths with a passion for technology and fostering economic development in Ohafia.
+                    </p>
+                </section>
+                <section class="flex items-start justify-between md:justify-end w-full">
+                    <div class="text-sm leading-8">
+                        <h1 class="font-bold text-x1 text-white pb-3">Services</h1>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact Us</li>
+                    </div>
+                    <div class="text-sm leading-8">
+                        <h1 class="font-bold text-x1 text-white pb-3">Contact Us</h1>
+                        <li>+234(0) 901 234 5678</li>
+                        <li>hellolenastride@gmail.com</li>
+                    </div>
+                </section>
+            </div>
+        </div>
+        <div class="w-full p-2 pb-5 gap-3 flex md:max-w-[100em] opacity-60">
+            <button class="rounded-[50em] border-2 w-9 h-9 flex justify-center items-center p-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#fff" d="M22 5.8a8.5 8.5 0 0 1-2.36.64a4.13 4.13 0 0 0 1.81-2.27a8.2 8.2 0 0 1-2.61 1a4.1 4.1 0 0 0-7 3.74a11.64 11.64 0 0 1-8.45-4.29a4.16 4.16 0 0 0-.55 2.07a4.09 4.09 0 0 0 1.82 3.41a4.05 4.05 0 0 1-1.86-.51v.05a4.1 4.1 0 0 0 3.3 4a4 4 0 0 1-1.1.17a5 5 0 0 1-.77-.07a4.11 4.11 0 0 0 3.83 2.84A8.22 8.22 0 0 1 3 18.34a8 8 0 0 1-1-.06a11.57 11.57 0 0 0 6.29 1.85A11.59 11.59 0 0 0 20 8.45v-.53a8.4 8.4 0 0 0 2-2.12"/></svg>
+            </button>
+            <button class="rounded-[50em] border-2 w-9 h-9 flex justify-center items-center p-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#fff" d="M14 13.5h2.5l1-4H14v-2c0-1.03 0-2 2-2h1.5V2.14c-.326-.043-1.557-.14-2.857-.14C11.928 2 10 3.657 10 6.7v2.8H7v4h3V22h4z"/></svg>
+            </button>
+            <button class="rounded-[50em] border-2 w-9 h-9 flex justify-center items-center p-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path fill="#fff" d="M5 3c0 1.062-.71 1.976-2.001 1.976C1.784 4.976 1 4.114 1 3.052C1 1.962 1.76 1 3 1s1.976.91 2 2M1 19V6h4v13zm6-8.556c0-1.545-.051-2.836-.102-3.951h3.594l.178 1.723h.076c.506-.811 1.746-2 3.822-2C17.1 6.216 19 7.911 19 11.558V19h-4v-6.861c0-1.594-.607-2.81-2-2.81c-1.062 0-1.594.86-1.873 1.569c-.102.254-.127.608-.127.963V19H7z"/></svg>
+            </button>
+            <button class="rounded-[50em] border-2 w-9 h-9 flex justify-center items-center p-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path fill="#fff" d="M8.617 13.227C8.091 15.981 7.45 18.621 5.549 20c-.586-4.162.861-7.287 1.534-10.605c-1.147-1.93.138-5.812 2.555-4.855c2.975 1.176-2.576 7.172 1.15 7.922c3.891.781 5.479-6.75 3.066-9.199C10.369-.275 3.708 3.18 4.528 8.245c.199 1.238 1.478 1.613.511 3.322c-2.231-.494-2.897-2.254-2.811-4.6c.138-3.84 3.449-6.527 6.771-6.9c4.201-.471 8.144 1.543 8.689 5.494c.613 4.461-1.896 9.293-6.389 8.945c-1.218-.095-1.728-.699-2.682-1.279"/></svg>
+            </button>
+        </div>
+        <div class="w-full flex items-center text-sm py-10 pt-5 border-t-2 border-gray-700 justify-between md:max-w-[100em] text-white">
+                <p class="flex justify-around w-full">
+                    <span class="block md:w-full"> © 2024 Innovate Ohafia</span>
+                    <a href="#" class="text-white md:w-40">Privacy Policy</a>
+                    <a class="text-white md:w-40" href="#">Term of Service</a>
+                </p>
+        </div>
+
+    </footer>
+</template>
