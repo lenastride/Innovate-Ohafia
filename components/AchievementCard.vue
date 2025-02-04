c79bade3bfc974b140f17b26d0fe89124105911a
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2025-02-04 07:52:09 +0100

    work on the AchivementCard component and make it responsive

--- components/AchievementCard.vue
+++ components/AchievementCard.vue
@@ -6,11 +6,11 @@
 </script>
 
 <template>
-    <div class="w-full max-w-[23rem] p-4 border border-[#FF0000]">
+    <div class="w-full min-w-[19rem] p-4 border border-[#FF0000]">
        <section class="relative">
-        <img src="/achievement-1.png" alt="">
+        <img src="/achievement-1.png" alt=""v>
         <p class="bg-white px-4 py-2 text-[#FF0000] text-xl font-bold absolute bottom-2">{{ title }}</p>
        </section>
-       <p class="pt-4">{{ description }}</p> 
+       <p class="pt-4">{{ description }}</p>
     </div>
 </template>
