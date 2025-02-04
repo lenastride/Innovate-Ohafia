33ab1216696e3019aa2c4b3f941b933197b04200
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2025-02-04 07:55:07 +0100

    work on the WhyAttendCard component and make it responsive

--- components/WhyAttendCard.vue
+++ components/WhyAttendCard.vue
@@ -7,11 +7,11 @@
 </script>
 
 <template>
-    <div class="w-full max-w-[40rem] p-4 border border-[#FFC2C2] flex flex-col gap-4">
+    <div class="w-full max-w-[30em] p-4 border border-[#FFC2C2] flex flex-col gap-4">
         <img :src="icon" alt="" class="w-20">
-        <section class="flex flex-col gap-2">
+        <section class="flex flex-col pt-4 gap-2">
             <h1 class="text-xl font-bold">{{ title }}</h1>
-            <p>{{ description }}</p>
+            <p class="text-sm md:text-xl">{{ description }}</p>
         </section>
     </div>
 </template>
