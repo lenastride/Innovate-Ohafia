460ecc5132d73196e975427f9b749638208f415a
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2025-02-04 07:49:19 +0100

    add the JoinComunity component

---
+++ components/JoinCommunity.vue
@@ -0,0 +1,8 @@
+
+
+<template>
+    <section class="px-[1em] py-[2em] mt-5 bg-[url(./hero-background.png)] flex flex-col justify-center items-center text-white gap-4">
+        <h1 class="text-2xl text-center font-bold">Click here to join the community</h1>
+        <button class="bg-[#D90000] p-2 px-4">Donate</button>
+    </section>
+</template>
