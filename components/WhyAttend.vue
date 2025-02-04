fd4d12ec4e91584cbf6d960be383095b21666ff9
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2025-02-04 07:54:55 +0100

    work on the WhyAttend component and make it responsive

--- components/WhyAttend.vue
+++ components/WhyAttend.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="w-full py-14 flex justify-center items-center">
-    <div class="w-full max-w-[60vw] flex flex-col justify-center gap-4">
-        <h1 class="font-bold text-3xl text-[#D90000] text-left">Why you should attend</h1>
-        <div class="grid grid-cols-2 gap-10">
+    <div class="w-full max-w-[70em] flex flex-col justify-center gap-4">
+        <h1 class="font-bold text-2xl md:text-3xl text-[#D90000] ml-[6.3%] text-left">Why you should attend</h1>
+        <div class="flex flex-wrap gap-8 px-5 md:px-0 justify-center">
             <WhyAttendCard icon="/attend-1.png" title="Engagement" description="#InnovateOhafia conferences bring together professionals, experts, and industry leaders from diverse backgrounds. This environment presents excellent networking opportunities, allowing you to meet and connect with like-minded individuals, potential clients, collaborators, and mentors." />
             <WhyAttendCard icon="/attend-2.png" title="Knowledge and skill enhancement" description="#InnovatOhafia Conferences offer a platform for industry experts to share their knowledge, insights, and best practices. Gaining new knowledge and skills can enhance your professional expertise and help you stay ahead of the curve." />
             <WhyAttendCard icon="/attend-3.png" title="Industry updates and insights" description="#InnovateOhafia conferences provide an opportunity to stay up-to-date with industry news, trends, and regulations. This knowledge can help you anticipate changes, adapt your strategies, and make informed decisions." />
