4d321c805672406b8caa2f5d32d5a4f561e1c8a4
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2025-02-04 07:51:54 +0100

    work on the Achivement component and make it responsive

--- components/Achievement.vue
+++ components/Achievement.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="bg-[#F5F5F5] w-full py-14 flex flex-col items-center">
-    <div class="w-full max-w-[60vw] flex flex-col justify-center gap-4">
-        <h1 class="font-bold text-3xl text-[#D90000] text-left">How do we achieve this?</h1>
-        <div class="flex justify-center gap-8">
+    <div class="w-full max-w-[70em] flex flex-col justify-center gap-4">
+        <h1 class="font-bold text-2xl md:text-3xl text-[#D90000] ml-[6.3%] text-left">How do we achieve this?</h1>
+        <div class="flex ml:justify-center w-full overflow-auto px-[1.5em] md:px-[4em] gap-8">
             <AchievementCard title="Tech Skills Development" description="Equip participants with in-demand technical skills through workshops, hackathons, and mentorship programs. " />
             <AchievementCard title="Tech Skills Development" description="Equip participants with in-demand technical skills through workshops, hackathons, and mentorship programs. " />
             <AchievementCard title="Tech Skills Development" description="Equip participants with in-demand technical skills through workshops, hackathons, and mentorship programs. " />
