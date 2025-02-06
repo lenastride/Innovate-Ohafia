a5414b07b0aebef60ce3627fbed25a9fc9191deb
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2025-02-06 18:31:17 +0100

    fix content

--- components/ProgrammeDetails.vue
+++ components/ProgrammeDetails.vue
@@ -2,7 +2,7 @@
     <div class="bg-white w-full max-w-[60rem] hidden md:flex px-8 py-10 justify-center items-center text-black absolute -bottom-16 shadow-2xl">
         <section class="px-10 border-r border-[#FF0000] text-center z-10">
             <p>DATE</p>
-            <h1>14th Oct. 2025</h1>
+            <h1>7th - 8th Aug. 2025</h1>
         </section>
         <section class="px-10 border-r border-[#FF0000] text-center z-10">
             <p>TIME</p>
@@ -10,7 +10,7 @@
         </section>
         <section class="px-10 text-center z-10">
             <p>VENUE</p>
-            <h1>Ohafia, Rivers State</h1>
+            <h1>Ohafia, Abia State</h1>
         </section>
             <img src="/details-1.svg" class="absolute right-0 bottom-0" alt="">
             <img src="/details-2.svg" class="absolute h-full left-0 bottom-0" alt="">
