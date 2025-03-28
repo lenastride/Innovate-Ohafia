f655e6147ba6511b7743594f5153456452dd673e
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2025-03-28 13:37:07 +0100

    complete the ProgrammeDetials section

--- components/ProgrammeDetails.vue
+++ components/ProgrammeDetails.vue
@@ -1,25 +1,27 @@
 <template>
-    <div class="bg-white w-full max-w-[60rem] hidden md:flex px-8 py-10 justify-center items-center text-black absolute -bottom-16 shadow-2xl">
-        <section class="px-10 border-r border-[#FF0000] text-center z-10">
-            <p>DATE</p>
-            <h1>7th - 8th Aug. 2025</h1>
-        </section>
-        <section class="px-10 border-r border-[#FF0000] text-center z-10">
-            <p>TIME</p>
-            <h1>9:00AM</h1>
-        </section>
-        <section class="px-10 text-center z-10">
-            <p>VENUE</p>
-            <h1>Ohafia, Abia State</h1>
-        </section>
-            <img src="/details-1.svg" class="absolute right-0 bottom-0" alt="">
-            <img src="/details-2.svg" class="absolute h-full left-0 bottom-0" alt="">
-    </div>
+  <div
+    class="bg-white w-full max-w-[60rem] hidden md:flex px-8 py-10 justify-center items-center text-black absolute -bottom-16 shadow-2xl"
+  >
+    <section class="px-10 border-r border-[#FF0000] text-center z-10">
+      <p>DATE</p>
+      <h1>7th - 8th Aug. 2025</h1>
+    </section>
+    <section class="px-10 border-r border-[#FF0000] text-center z-10">
+      <p>TIME</p>
+      <h1>9:00AM</h1>
+    </section>
+    <section class="px-10 text-center z-10">
+      <p>VENUE</p>
+      <h1>Ohafia, Abia State</h1>
+    </section>
+    <img src="/details-1.svg" class="absolute right-0 bottom-0" alt="" />
+    <img src="/details-2.svg" class="absolute h-full left-0 bottom-0" alt="" />
+  </div>
 </template>
 
 <style scoped>
-    h1{
-        font-size: 1.5rem;
-        font-weight: bold;
-    }
+h1 {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
 </style>
