d748e61dc110d56d253d77508fe03a76d555ee6f
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2025-03-28 13:37:28 +0100

    complet ethe contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,1 +1,14 @@
-<template></template>
+<template>
+  <div>
+    <section
+      class="px-[1em] py-[5em] mt-5 bg-[url(./hero-background.png)] flex flex-col justify-center items-center text-white gap-4"
+    >
+      <h1 class="text-2xl text-center font-bold">
+        Contact <span class="text-[#D90000]">Us</span>
+      </h1>
+    </section>
+    <Form />
+    <Testimonials />
+  </div>
+</template>
+
