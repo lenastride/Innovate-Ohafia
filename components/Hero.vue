79e74d5e213eccd166faef885d33939d727c7cd9
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2025-03-28 13:35:44 +0100

    fixup the Hero component

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,18 +1,28 @@
 <template>
-    <div class="h-[90vh] px-[1em]  bg-[url(./hero-background.png)] flex flex-col justify-center items-center text-white gap-4">
-        <h1 class="max-w-[40rem] text-[2rem] md:text-[60px] text-center font-bold leading-[1.6em] md:leading-[1.3em]">
-            Igniting <span>Innovation</span> and Empowering <span>Futures</span>
-        </h1>
-        <p class="max-w-[30em] text-sm md:text-lg text-center">Igniting youths with a passion for technology and fostering economic development in <span class="text-[#D90000] !decoration-1 !underline-offset-1">Ohafia</span>.</p>
-        <ProgrammeDetails />
-    </div>
+  <div
+    class="h-[90vh] px-[1em] bg-[url(./hero-background.png)] bg-cover bg-no-repeat flex flex-col justify-center items-center text-white gap-4"
+  >
+    <h1
+      class="max-w-[40rem] text-[2rem] md:text-[60px] text-center font-bold leading-[1.6em] md:leading-[1.3em]"
+    >
+      Igniting <span>Innovation</span> and Empowering <span>Futures</span>
+    </h1>
+    <p class="max-w-[30em] text-sm md:text-lg text-center">
+      Igniting youths with a passion for technology and fostering economic
+      development in
+      <span class="text-[#D90000] !decoration-1 !underline-offset-1"
+        >Ohafia</span
+      >.
+    </p>
+    <ProgrammeDetails />
+  </div>
 </template>
 
 <style scoped>
-    span{
-        color: #FF2E2E;
-        text-decoration: underline;
-        text-underline-offset: .5rem;
-        text-decoration-thickness: .2rem;
-    }
+span {
+  color: #ff2e2e;
+  text-decoration: underline;
+  text-underline-offset: 0.5rem;
+  text-decoration-thickness: 0.2rem;
+}
 </style>
