91fdb016fb0bb5aa54eee0ad3152f3d06e183896
umeklinks <umekalu@gmail.com>
2025-10-31 00:05:25 +0100

    new page

---
+++ pages/tickets.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Ticket - Innovate Ohafia',
+  description: 'Join Innovate Ohafia in transforming our community through technology education, innovation hubs, and youth empowerment programs.',
+  ogTitle: 'Innovate Ohafia - Home',
+  ogDescription: 'Join Innovate Ohafia in transforming our community through technology education and youth empowerment.',
+  ogImage: '/og-image.jpg',
+  ogUrl: 'https://innovateohafia.com',
+  twitterTitle: 'Innovate Ohafia - Home',
+  twitterDescription: 'Transforming Ohafia through technology and innovation.',
+  twitterImage: '/og-image.jpg'
+})
+
+useHead({
+  link: [
+    { rel: 'canonical', href: 'https://innovateohafia.com' }
+  ]
+})
+</script>
+
+<template>
+    <div>
+        <p></p>
+    </div>
+</template>
