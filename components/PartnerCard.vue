eb0decabbdba344eadf60c22ff9005ff3f7cf30e
Abraham kalu Ume <umekalu@gmail.com>
2026-08-08 00:02:39 +0100

    Updated

--- components/PartnerCard.vue
+++ components/PartnerCard.vue
@@ -10,7 +10,7 @@
     <div class="relative w-full md:px-4 md:max-w-[27em] max-w-[18em]">
     <div class="bg-white relative min-h-[13.5em] w-full flex items-center px-3 py-8 md:px-12 md:py-12 text-[1rem] md:text-x1 shadow-xl">
     <h1
-      class="absolute text-white text-stroke text-[4.5rem] font-bold md:hidden"
+      class="absolute text-white text-stroke text-[4.5rem] font-bold"
       :class="{
         'bottom-[-25%]': titlePosition === 'bottom',
         'top-[-25%]': titlePosition === 'top',
--- components/Partners.vue
+++ components/Partners.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="py-20 md:pt-40">
-    <section class="flex flex-col gap-8">
+    <!-- <section class="flex flex-col gap-8">
         <h1 class="text-[2.5rem] text-center font-bold">Our Partners</h1>
         <div class="w-full bg-[#F5F5F5] p-8 flex justify-evenly">
             <div class="w-[70em] justify-around items-center flex">
@@ -18,7 +18,7 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> -->
 
     <section class=" mt-[3em] py-[4.5em] overflow-x-hidden flex-col justify-center items-center md:flex-row flex gap-10">
         <PartnerCard titlePosition="top" title="Vision" text="To bridge the digital divide in Ohafia by empowering youth with the skills, resources, and connections needed to become active participants in the global tech landscape." />
