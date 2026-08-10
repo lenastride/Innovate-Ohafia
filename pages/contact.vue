3dba4c2214e8ebeb16397bf439bc8975770b20bc
Abraham kalu Ume <umekalu@gmail.com>
2026-08-10 12:39:29 +0100

    Removed dot from image URL

--- pages/contact.vue
+++ pages/contact.vue
@@ -21,7 +21,7 @@ useHead({
 <template>
   <div>
     <section
-      class="px-[1em] py-[5em] mt-5 bg-[url(./hero-background.png)] flex flex-col justify-center items-center text-white gap-4"
+      class="px-[1em] py-[5em] mt-5 bg-[url(/hero-background.png)] flex flex-col justify-center items-center text-white gap-4"
     >
       <h1 class="text-2xl text-center font-bold">
         Contact <span class="text-[#D90000]">Us</span>
